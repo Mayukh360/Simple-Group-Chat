05ef31294a6749d55123b991f5ae4d4fb4a40c73
Mayukh <mayukhchatterjee2023@gmail.com>
2023-06-16 18:02:33 +0530

    changes

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 const app = express();
+const cookieParser = require("cookie-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // Create a variable to store the messages
 let messages = [];
 
-app.use('/login', (req, res, next) => {
-  res.send('<form method="POST" action="/chat" ><label>Name</label><input type="text" name="username"/><button type="submit">Login</button></form>');
+app.use("/login", (req, res, next) => {
+  res.send(
+    '<form method="POST" action="/chat" ><label>Name</label><input type="text" name="username"/><button type="submit">Login</button></form>'
+  );
 });
 
-app.use('/chat', (req, res, next) => {
+app.use("/chat", (req, res, next) => {
   // Display the chat
-  let chatDisplay = '';
+  let chatDisplay = "";
   const username = req.body.username;
-//   saveUsernameToFile(username); // Save username to a text file
-  messages.forEach(entry => {
+  res.cookie("username", username);
+  //save the username in session storage
+  //   saveUsernameToFile(username); // Save username to a text file
+  messages.forEach((entry) => {
     chatDisplay += `${entry.username}: ${entry.message}<br>`;
   });
 
@@ -43,36 +49,35 @@ app.use('/chat', (req, res, next) => {
 //       }
 //     });
 //   }
-  
-  app.use('/sent', (req, res, next) => {
-    // Get the username and message from the request body
-    const username = req.body.username;
-    const message = req.body.message;
-  
-    // Save the username to the username.txt file
-    // saveUsernameToFile(username);
-  
-    // Create an object to store the username and message
-    const chatEntry = {
-      username,
-      message
-    };
-  
-    // Add the chat entry to the array
-    messages.push(chatEntry);
-  
-    // Log the chat entry to the console
-    console.log('CHAT', chatEntry);
-  
-    // Save the messages to a text file
-    // saveMessagesToFile();
-  
-    res.redirect('/chat');
-  });
-  
-  
-  
-// Function to save the username to a text file
 
+app.use("/sent", (req, res, next) => {
+  // Get the username and message from the request body
+  const username = req.body.username;
+  const message = req.body.message;
+
+  
+
+  // Save the username to the username.txt file
+  // saveUsernameToFile(username);
+
+  // Create an object to store the username and message
+  const chatEntry = {
+    username,
+    message,
+  };
+
+  // Add the chat entry to the array
+  messages.push(chatEntry);
+
+  // Log the chat entry to the console
+  console.log("CHAT", chatEntry);
+
+  // Save the messages to a text file
+  // saveMessagesToFile();
+
+  res.redirect("/chat");
+});
+
+// Function to save the username to a text file
 
 app.listen(3000);
